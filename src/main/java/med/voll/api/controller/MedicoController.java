a61d54e24683807ff288ab3818289380caeb3c62
nsfge <nsfgeo@hotmail.com>
2023-11-22 22:24:05 -0300

    criando funcionalidade para listar medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
@@ -21,8 +25,14 @@ public class MedicoController {
 	private MedicoRepository repository;
 
 	@PostMapping
-	@Transactional
+	@Transactional		// esse eu uso quando for adicionar, excluir ou editar informações do banco de dados
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 		repository.save(new Medico(dados));
+	}
+	
+	@GetMapping
+	public List<DadosListagemMedico> listar(){
+		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+		// para nao retornar todos os dados que compoem o cadastro do médico eu faço um DTO (DadosListagemMedico) para os parametros que desejo exibir, trago para cá e converto o Médico que recebo pelo Repository em DadosCadastroMédico.
 	}
 }
